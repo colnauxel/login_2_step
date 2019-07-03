b1d11b6ab943e634cc9d1706096d5b24502eba19
colnaxuel <xuanloc120297@gmail.com>
2019-07-03 22:32:05 +0700

    login_2_steps

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 from flask import Flask, render_template, request, flash, url_for, redirect, session
 from functools import wraps
 from flask_pymongo import PyMongo
-from pymongo import MongoClient
-from passlib.hash import sha256_crypt,md5_crypt
-from bson.json_util import dumps
-import  json
+from passlib.hash import sha256_crypt
+
 import pyotp
 
-from wtforms import Form, StringField
 app = Flask(__name__)
 
-# app.config['MONGO_DBNAME'] = 'data_login'
-# app.config['MONGO_URI'] = 'mongodb://localhost:27017/data_login'
-# mongo = PyMongo(app)
-
-client = MongoClient('mongodb://localhost:27017')
-db = client['data_login']
+app.config['MONGO_DBNAME'] = 'data_login'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/data_login'
+mongo = PyMongo(app)
 
 @app.route('/')
 def home():
@@ -27,19 +21,17 @@ def login():
         # get Form fields
         username = request.form['username']
         password = request.form['password']
-        user = db.users
+        user = mongo.db.users
         u = user.find({'username': username})
         arr_user = []
         for user in u:
             arr_user.append(user)
-        # data = dumps(u)
-        # d=data['email']
-        # test= [{"email":"xuanloc120297@gmail.com"}]
         if u.count() > 0:
             pass_db = str(arr_user[0]['password'])
             if sha256_crypt.verify(password,pass_db):
                 session['username'] = username
                 session['email'] = arr_user[0]['email']
+                session['key_otp'] = arr_user[0]['key_otp']
                 session['logged_in'] = True
                 flash('Login user and password success'+str(arr_user[0]['password']), 'success')
                 return redirect(url_for('otp'))
@@ -77,18 +69,18 @@ def otp():
         data_qr = request.form['data_qr']
         code_otp = request.form['code_otp']
         otp_totp = pyotp.TOTP(code_otp)
+
         if data_qr == otp_totp.now():
             session['verify_otp'] = True
             flash("verify code success", 'success')
             return redirect(url_for('dashboard'))
         create_qr = request.form['create_qr']
         error = "code OTP incorrect!"
-        return  render_template('otp.html', error = error, create_qr = create_qr, secret_key = code_otp)
+        return  render_template('otp.html', error = error, secret_key = code_otp)
     # Create OTP
-    secret_key = pyotp.random_base32()
-    create_qr = pyotp.totp.TOTP(secret_key).provisioning_uri(session['email'], issuer_name = "Secure App")
-    totp = pyotp.TOTP(secret_key)
-    return render_template('otp.html', secret_key = secret_key, create_qr = create_qr)
+    secret_key = session['key_otp']
+    create_qr = pyotp.totp.TOTP(secret_key).provisioning_uri(session['email'], issuer_name = "Login 2 step")
+    return render_template('otp.html', secret_key = secret_key)
 
 @app.route('/dashboard')
 @is_verify_otp
